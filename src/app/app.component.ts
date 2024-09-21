e185dda58e12e57cf80fb69fceef96d61c56ad3c
alberto sanchez <alberto.sanchez@reinamadre.mx>
2024-09-21 14:19:56 -0500

    feat(main): implement main view structure and behavior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {TranslateService} from "@ngx-translate/core";
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatAnchor} from "@angular/material/button";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, MatAnchor, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'learning-center';
+  options = [
+    { path: '/home', title: 'Home' },
+    { path: '/learning/courses', title: 'Courses' },
+    { path: '/about', title: 'About' },
+  ];
 
   constructor(private translateService: TranslateService) {
     this.translateService.use('en');
