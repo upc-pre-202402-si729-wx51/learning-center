e0a59ffa6ec8f781745da72ce15b68ce2359143c
alberto sanchez <alberto.sanchez@reinamadre.mx>
2024-09-21 14:03:07 -0500

    feat(public): add page not found component

---
+++ src/app/public/pages/page-not-found/page-not-found.component.ts
@@ -0,0 +1,30 @@
+import {Component, inject, OnInit} from '@angular/core';
+import {MatButton} from "@angular/material/button";
+import {ActivatedRoute, Router} from "@angular/router";
+
+@Component({
+  selector: 'app-page-not-found',
+  standalone: true,
+  imports: [
+    MatButton
+  ],
+  templateUrl: './page-not-found.component.html',
+  styleUrl: './page-not-found.component.css'
+})
+export class PageNotFoundComponent implements OnInit {
+  protected invalidUrl: string;
+  private route: ActivatedRoute = inject(ActivatedRoute);
+  private router: Router = inject(Router);
+
+  constructor() {
+    this.invalidUrl = '';
+  }
+
+  ngOnInit(): void {
+    this.invalidUrl = this.route.snapshot.url[0].path;
+  }
+
+  protected onNavigateHome() {
+    this.router.navigate(['home']).then();
+  }
+}
